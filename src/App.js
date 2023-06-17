c861cac3cf79a4b7eddabdffb2a91c65928e1c4f
seenih <ibrahim_adamsha@yahoo.com>
2023-06-17 01:30:23 +0000

    Add new blog version

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog  EKS DEMO Blog version
+            EKS DEMO Blog  Ibby Adamsha
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
